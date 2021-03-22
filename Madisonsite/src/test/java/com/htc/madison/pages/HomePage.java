789f4947833eb219444c56dfe0c0589fdb9df7bd
h <h@hp>
2021-03-22 16:58:56 +0530

    commite

---
+++ Madisonsite/src/test/java/com/htc/madison/pages/HomePage.java
@@ -0,0 +1,51 @@
+package com.htc.madison.pages;
+
+import java.util.NoSuchElementException;
+
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.htc.madison.utilites.Utility;
+
+
+
+public class HomePage extends BasePage{
+	//private WebDriver driver = null;
+	@FindBy(xpath = "//div[@class='block-content']//ul//li[@class='current']//a//strong")
+	private WebElement dashboardBy;
+	@FindBy(xpath = "//strong[contains(text(),\"Hello, prashant shivaji more!\")]")
+	private WebElement assertcheckBy;
+	@FindBy(xpath = "//input[@id='search']")
+	private WebElement searchproducts;
+	@FindBy(xpath = "//button[@class='button search-button']")
+	private WebElement clicksearch;
+
+	public HomePage(WebDriver driver) {
+		super(driver);
+		this.driver = driver;
+	}
+
+	public void Home_Page(String search) {
+		try {
+			Utility.waitToBeVisibile(dashboardBy, driver);
+			dashboardBy.click();
+			searchproducts.click();
+			searchproducts.sendKeys(search);
+			clicksearch.click();
+		} catch (NoSuchElementException | StaleElementReferenceException exception) {
+			System.out.println(exception.getStackTrace());
+		}
+	}
+
+	public WebDriver driver() {
+		return driver;
+	}
+
+	public String assert_login() {
+		return assertcheckBy.getText();
+
+	}
+}
